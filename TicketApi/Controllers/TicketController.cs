6490a754751896378057f4014d19c1af78041370
kosaj <kosaj@users.noreply.github.com>
2022-07-19 14:24:39 +0200

    .

---
+++ TicketApi/Controllers/TicketController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketApi {
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase {
+        
+    }
+}
